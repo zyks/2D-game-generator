36c9e63188df4ad338cb9cea1cd2c791684050b7
Paweł Zykowski <pazy011@gmail.com>
2016-12-22 19:28:51 +0100

    Use html as views extensions

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var http = require('http');
 var express = require('express');
+var ejs = require('ejs');
 
 var app = express();
 app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
 
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { message: "Hello world!" });
 });
-
 
 var server = http.createServer(app).listen(8000);
 console.log("=> Server started");

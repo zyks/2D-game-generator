20695e213b6e06c752d2d832336c5357533c0f48
Paweł Zykowski <pazy011@gmail.com>
2016-12-23 15:36:53 +0100

    Add method for fetching all entity's components

--- engine/entityComponents.js
+++ engine/entityComponents.js
@@ -1,22 +1,29 @@
 var EntityComponents = function(components=[]) {
-    this.components = {};
+    this._componentsByName = {};
     for (component of components)
         this.add(component);
 }
 
 EntityComponents.prototype.add = function(component) {
-    if (!(component.name in this.components))
-        this.components[component.name] = component;
+    if (!(component.name in this._componentsByName))
+        this._componentsByName[component.name] = component;
 }
 
 EntityComponents.prototype.remove = function(component) {
-    if (component.name in this.components) 
-        delete this.components[component.name];
+    if (component.name in this._componentsByName) 
+        delete this._componentsByName[component.name];
 }
 
 EntityComponents.prototype.get = function(name) {
-    if (name in this.components)
-        return this.components[name];
+    if (name in this._componentsByName)
+        return this._componentsByName[name];
+}
+
+EntityComponents.prototype.all = function() {
+    var components = [];
+    for (name in self._componentsByName)
+        components.concat(this._componentsByName[name]);
+    return components;
 }
 
 

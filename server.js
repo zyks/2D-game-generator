01c83fbf75723cdc9c63465cfe19f19eefe0e7a9
Paweł Zykowski <pazy011@gmail.com>
2016-12-26 13:26:59 +0100

    [#7] Use socket.io for client-server websocket communication

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 var http = require('http');
 var express = require('express');
 var ejs = require('ejs');
+var socket = require('socket.io')
 
 
 var Server = function() {
     this._app = express();
+    this._server = http.createServer(this._app);
+    this._io = socket(this._server);
 }
 
 Server.prototype.configure = function() {
-    this._app.set('view engine', 'ejs');
     this._app.set('view engine', 'html');
     this._app.engine('html', ejs.renderFile);
+    this._app.set('views', './views');
+    this._app.use(express.static('./'));
+    this._io.on('connection', this._registerPlayer);
 }
 
 Server.prototype.setRouting = function() {
@@ -20,9 +25,12 @@ Server.prototype.setRouting = function() {
 }
 
 Server.prototype.run = function(port) {
-    this._server = http.createServer(this._app);
     this._server.listen(port);
     console.log("=> Server started");
+}
+
+Server.prototype._registerPlayer = function(socket) {
+    console.log("new player connected");
 }
 
 

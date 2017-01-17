467b3ff377713c21283bcc33c67359e2d4b0a370
Paweł Zykowski <pazy011@gmail.com>
2017-01-17 23:36:30 +0100

    [#5] Add system for sending game state to players

---
+++ systems/sendGameStateSystem.js
@@ -0,0 +1,47 @@
+var SendGameStateSystem = function(engine) {
+    this._engine = engine;
+    this._elapsedTime = 0;
+    this._sendingInterval = 100;
+}
+
+SendGameStateSystem.prototype.start = function() {
+
+}
+
+SendGameStateSystem.prototype.update = function(time) {
+    this._elapsedTime += time;
+
+    if (this._elapsedTime < this._sendingInterval)
+        return;
+
+    this._elapsedTime -= this._sendingInterval;
+    let gameState = this._getGameState();
+    let gameStateString = this._serialize(gameState);
+
+    for (let player of gameState.players) {
+        let playerInfo = player.components.get('PlayerInfo');
+        playerInfo.socket.emit('gameState', gameStateString);
+    }
+}
+
+SendGameStateSystem.prototype.end = function() {
+
+}
+
+SendGameStateSystem.prototype._getGameState = function() {
+    let gameState = {
+        players: this._engine.entities.getByGroup('players')
+    }
+    return gameState;
+}
+
+SendGameStateSystem.prototype._serialize = function(data) {
+    let serializedData = JSON.stringify(data, (key, value) => {
+        if(key !== 'socket')
+            return value;
+    });
+    return serializedData;
+}
+
+
+module.exports = SendGameStateSystem;

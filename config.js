eb52368684cfae3984edadea52bc3e05b8bd5659
Krzysztof Drab <krabex93@gmail.com>
2017-01-22 18:38:38 +0100

    [#14] Extract constans to config module

---
+++ config.js
@@ -0,0 +1,7 @@
+var Config = new (function() {
+    this.TILE_SIZE = 48;
+    this.GAME_WIDTH = 600;
+    this.GAME_HEIGHT = 800;
+});
+
+module.exports = Config;
--- systems/tileMapRenderSystem.js
+++ systems/tileMapRenderSystem.js
@@ -1,10 +1,9 @@
+var Config = require('../Config');
+
 var TileMapRenderSystem = function(engine, ctx, atlas) {
     this._engine = engine;
     this._ctx = ctx;
     this._atlas = atlas;
-    this.TILE_SIZE = 48;
-    this.GAME_WIDTH = 800;
-    this.GAME_HEIGHT = 600;
 }
 
 
@@ -26,20 +25,20 @@ TileMapRenderSystem.prototype.end = function() {
 }
 
 TileMapRenderSystem.prototype._getViewportContext = function(camera, layer) {
-    var ViewportContext = function(camera, map, TILE_SIZE, GAME_WIDTH, GAME_HEIGHT) {
+    var ViewportContext = function(camera, map) {
         cameraPosition = camera.components.get("Position");
-        this.viewportXStart = cameraPosition.x - Math.floor(GAME_WIDTH / 2);
+        this.viewportXStart = cameraPosition.x - Math.floor(Config.GAME_WIDTH / 2);
         this.viewportXStart = Math.max(0, this.viewportXStart);
-        this.viewportXStart = Math.min(layer.width * TILE_SIZE - GAME_WIDTH, this.viewportXStart);
-        this.viewportYStart = cameraPosition.y - Math.floor(GAME_HEIGHT / 2);
+        this.viewportXStart = Math.min(layer.width * Config.TILE_SIZE - Config.GAME_WIDTH, this.viewportXStart);
+        this.viewportYStart = cameraPosition.y - Math.floor(Config.GAME_HEIGHT / 2);
         this.viewportYStart = Math.max(0, this.viewportYStart);
-        this.viewportYStart = Math.min(layer.height * TILE_SIZE - GAME_HEIGHT, this.viewportYStart);
-        this.startXTile = Math.floor(this.viewportXStart / TILE_SIZE);
-        this.startYTile = Math.floor(this.viewportYStart / TILE_SIZE);
-        this.viewportXOffset = this.startXTile * TILE_SIZE - this.viewportXStart;
-        this.viewportYOffset = this.startYTile * TILE_SIZE - this.viewportYStart;
+        this.viewportYStart = Math.min(layer.height * Config.TILE_SIZE - Config.GAME_HEIGHT, this.viewportYStart);
+        this.startXTile = Math.floor(this.viewportXStart / Config.TILE_SIZE);
+        this.startYTile = Math.floor(this.viewportYStart / Config.TILE_SIZE);
+        this.viewportXOffset = this.startXTile * Config.TILE_SIZE - this.viewportXStart;
+        this.viewportYOffset = this.startYTile * Config.TILE_SIZE - this.viewportYStart;
     }
-    return new ViewportContext(camera, layer, this.TILE_SIZE, this.GAME_WIDTH, this.GAME_HEIGHT);
+    return new ViewportContext(camera, layer);
 };
 
 TileMapRenderSystem.prototype._renderTiles = function(layer, camera) {
@@ -50,17 +49,17 @@ TileMapRenderSystem.prototype._renderTiles = function(layer, camera) {
         for(let column = 0 ; column < 19 ; column++) {
             tile = layer.tiles[column + viewport.startXTile][row + viewport.startYTile];
             this._renderTile(tile, x, y);
-            x += this.TILE_SIZE;
+            x += Config.TILE_SIZE;
         }
-        y += this.TILE_SIZE;
+        y += Config.TILE_SIZE;
     }
 };
 
 TileMapRenderSystem.prototype._renderTile = function (tile, x, y) {
     this._ctx.drawImage(
         this._atlas,
-        this.TILE_SIZE * tile, 0, this.TILE_SIZE, this.TILE_SIZE,
-        x, y, this.TILE_SIZE, this.TILE_SIZE
+        Config.TILE_SIZE * tile, 0, Config.TILE_SIZE, Config.TILE_SIZE,
+        x, y, Config.TILE_SIZE, Config.TILE_SIZE
     );
 };
 

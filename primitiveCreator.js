f5ae39202faacf54494eb591b2677436b2783666
Krzysztof Drab <krabex93@gmail.com>
2017-01-28 17:34:12 +0100

    [#15] Add primitive creator

---
+++ primitiveCreator.js
@@ -0,0 +1,33 @@
+var Circle = require('./engine/gfx/circle');
+var Rect = require('./engine/gfx/rect');
+var PrimitivesGroup = require('./engine/gfx/primitivesGroup');
+
+var PrimitiveCreator = function(spritesRepository) {
+    this._spritesRepository = spritesRepository;
+    this._creators = {
+        player: this.createPlayerPrimitive
+    };
+}
+
+PrimitiveCreator.prototype.create = function(name) {
+    return this._creators[name]();
+}
+
+PrimitiveCreator.prototype.createPlayerPrimitive = function() {
+    let eye = [
+        new Circle(0, 0, 8).fill("white").stroke("black"),
+        new Circle(0, 0, 2).fill("blue"),
+    ]
+    let eyes = new PrimitivesGroup(0, -10, [
+        new PrimitivesGroup(-12, 0, eye),
+        new PrimitivesGroup(12, 0, eye)
+    ]);
+    let head = new PrimitivesGroup(0, 0, [
+        new Rect(-24, -24, 48, 48).fill("yellow").stroke("goldenrose", 2),
+        eyes,
+        new Rect(-8, 8, 16, 6).fill("red")
+    ]);
+    return head;
+}
+
+module.exports = PrimitiveCreator;

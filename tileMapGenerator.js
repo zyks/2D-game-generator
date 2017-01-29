ea6f74a7d426320f8123ac6a2af408e23a2457b7
Krzysztof Drab <krabex93@gmail.com>
2017-01-29 15:41:46 +0100

    [#18] Adjust celluar automata parameters. Put wall border around map

--- tileMapGenerator.js
+++ tileMapGenerator.js
@@ -7,23 +7,35 @@ TileMapGenerator.prototype.generate = function(width, height) {
     this._width = width;
     this._height = height;
     this.initMap()
-        .putWalls();
+        .randomize()
+        .cellularAutomata()
+        .putBorders();
     return this._map;
 }
 
-TileMapGenerator.prototype.putWalls = function() {
-  for(var i = 0; i < this._height; i++)
-      for(var j = 0; j < this._width; j++) {
-          this._map[i][j] = (Math.random() < 0.5 ? this.EMPTY_TILE : this.WALL);
-      }
-    this.cellular();
-    this.cellular();
-    this.cellular();
-    this.cellular();
+TileMapGenerator.prototype.putBorders = function() {
+    for(let i = 0 ; i < this._width ; i++)
+        this._map[0][i] = this._map[this._height-1][i] = this.WALL;
+    for(let i = 0 ; i < this._height ; i++)
+        this._map[i][0] = this._map[i][this._width-1] = this.WALL;
+    return this;
+}
+
+TileMapGenerator.prototype.randomize = function(emptyFactor = 0.5) {
+    for(var i = 0; i < this._height; i++)
+        for(var j = 0; j < this._width; j++) {
+            this._map[i][j] = (Math.random() < emptyFactor ? this.EMPTY_TILE : this.WALL);
+        }
     return this;
 };
 
-TileMapGenerator.prototype.cellular = function( ) {
+TileMapGenerator.prototype.cellularAutomata = function(iterations = 4) {
+    while(iterations--)
+        this._cellularAutomataStep();
+    return this;
+}
+
+TileMapGenerator.prototype._cellularAutomataStep = function() {
     let dirs = [
       { x: -1, y: -1},
       { x: -1, y:  0},
@@ -47,10 +59,10 @@ TileMapGenerator.prototype.cellular = function( ) {
                if(newY >= 0 && newY < this._height && newX >= 0 && newX < this._width && this._map[newX][newY])
                     walls += 1;
             }
-            if(this._map[j][i] == this.WALL && walls >= 4)
-                map[j][i] = this.WALL;
-            if(this._map[j][i] == this.EMPTY_TILE && walls >= 5)
-                map[j][i] = this.EMPTY_TILE;
+            if(this._map[j][i] == this.WALL)
+                map[j][i] = walls >= 4 ? this.WALL : this.EMPTY_TILE;
+            if(this._map[j][i] == this.EMPTY_TILE)
+                map[j][i] = walls > 4 ? this.WALL : this.EMPTY_TILE;
         }
     }
     this._map = map;
